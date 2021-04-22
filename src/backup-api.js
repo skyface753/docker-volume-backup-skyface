d371ef23b59d32b51bbd272c47a5f0972e443e64
Skyface <seppel.j@live.de>
2021-04-22 12:36:49 +0200

    Test API

--- src/backup-api.js
+++ src/backup-api.js
@@ -13,17 +13,17 @@ app.use(express.json());
 appLocal.use(express.urlencoded({ extended: false }));
 appLocal.use(express.json());
 
-var success, time;
+var time;
 
 appLocal.post('/setData', (req, res) =>{
     console.log("Get Data: " + req.body.success + " Time: " + req.body.Time);
-    success = req.body.success;
+    // success = req.body.success;
     time = req.body.time;
     res.send("Thanks for the Data");
 })
 
 app.get('/API', (req, res) =>{
-    res.send("Last Data: " + success + " at: " + time);
+    res.send("Last success at: " + time);
 })
 
 
